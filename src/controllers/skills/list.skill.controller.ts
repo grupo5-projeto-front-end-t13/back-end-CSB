f749caff767775ccbe980cdc49c89768369a1e9f
Bruno Paulin <bruno.p-@hotmail.com>
2023-01-12 12:02:31 -0300

    fix: skill controller

--- src/controllers/skills/list.skill.controller.ts
+++ src/controllers/skills/list.skill.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { listSkillsService } from "../../services/skills/skills.service";
 
 export const listSkillsController = async(req: Request, res: Response) => {
   const listSkills = await listSkillsService()
