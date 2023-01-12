c217fabbde10e06140e5ded5303c134444fa8462
rogeraguiar0 <aguiarroger02@gmail.com>
2023-01-11 21:28:59 -0300

    feat: testing list invites

--- src/middlewares/validateId.middleware.ts
+++ src/middlewares/validateId.middleware.ts
@@ -2,12 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/errors";
 import { userRepository } from "../repositories/userRepository";
 
+export const validateIdMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let id: string = req.params.id;
+  const user = await userRepository.findOneBy({ id: id });
 
-export const validateIdMiddleware  = async (req: Request, res: Response, next: NextFunction) => {
-      let id: string = req.params.id
-      const user = await userRepository.findOneBy({id: id})
-      
-      if(!user) throw new AppError (404, 'Invalid Id',)
+  if (!user) return res.status(404).json({ message: "Invalid Id" });
 
-      return next();
-    }
+  return next();
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,6 +6,9 @@ import { listBandsController } from "../controllers/band/listBands.controller";
 import { listMusiciansController } from "../controllers/user/listMusicians.controller";
 import { createUserInviteController } from "../controllers/user/userCreateInvite.controller";
 import { deleteController } from "../controllers/user/userDelete.controller";
+import { validateIdMiddleware } from "../middlewares/validateId.middleware";
+import { listUserReceivedInvitesController } from "../controllers/user/listUserReceivedInvites.controller";
+import { listUserSendedInvitesController } from "../controllers/user/listUserSendedInvites.controller";
 
 export const userRoutes = Router();
 
@@ -16,5 +19,15 @@ userRoutes.post(
 );
 userRoutes.get("/band", listBandsController);
 userRoutes.get("/musician", listMusiciansController);
+userRoutes.get(
+  "invites/received/:id",
+  validateIdMiddleware,
+  listUserReceivedInvitesController
+);
+userRoutes.get(
+  "invites/sended/:id",
+  validateIdMiddleware,
+  listUserSendedInvitesController
+);
 userRoutes.post("/invites", createUserInviteController);
 userRoutes.delete("/:id", deleteController);
