9081ffe8419f28ceec37e31852f0c67ced2c23bd
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-12 11:07:28 -0300

    feat: created middleware for token validation

---
+++ src/middlewares/validateAuthToken.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const validateAuthTokenMiddleware = async (req : Request, res : Response, next : NextFunction) => {
+    let authToken = req.headers.authorization;
+        
+    if(!authToken) res.status(401).json({ message: "Missing authorization headers" });
+    
+    authToken = authToken?.split(" ")[1];
+
+    return jwt.verify(authToken!, process.env.SECRET_KEY!, (error, decoded: any) => {
+        if(error) res.status(401).json({ message: "Missing authorization headers" });
+
+        req.user = {
+            id: decoded.sub,
+            type: decoded.type,
+            isAdm: decoded.isAdm
+        }
+
+        return next();
+    })
+}
