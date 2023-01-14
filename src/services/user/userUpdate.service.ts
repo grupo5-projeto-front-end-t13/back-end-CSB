abaef070d79c5b6362900b95fb1e0ef3cc8e0938
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-01-14 10:03:16 -0300

    fix: change where and relations to join

--- src/services/user/userUpdate.service.ts
+++ src/services/user/userUpdate.service.ts
@@ -5,27 +5,33 @@ import { userRepository } from "../../repositories/userRepository";
 import { createUserResponseSerializer } from "../../serializers/user/user.serializers";
 
 export const userUpdateService = async (userData:iUserUpdateRequest,id:string):Promise<iUserCreateResponse>=>{
-    const user = await userRepository.findOne({where:{id:id},relations:{skills:true}});
+  const user = await userRepository.createQueryBuilder("user")
+  .where("user.id = :id_user",{id_user:id})
+  .leftJoinAndSelect("user.skills","skills")
+  .getOne();
 
-    if(!user){
-        throw new AppError (404, 'User not found',);
-    }
+  if(!user){
+      throw new AppError (404, 'User not found',);
+  }
 
-    const { skills, ...data } = userData;
+  const { skills, ...data } = userData;
 
-    const findSkill =skills.id? await skillRepository.findOneBy({
-      id: skills.id,
-    }):user.skills;
+  const findSkill =skills.id? await skillRepository.findOneBy({
+    id: skills.id,
+  }):user.skills;
 
-    if(!findSkill){
-        throw new AppError (404, 'Skill not found',);
-    }
+  if(!findSkill){
+      throw new AppError (404, 'Skill not found',);
+  }
 
-    await userRepository.save({...user,...data,skills:{id:findSkill.id}});
+  await userRepository.save({...user,...data,skills:{id:findSkill.id}});
 
-    const updatedUser = await userRepository.findOne({where:{id:user.id},relations:{skills:true}});
+  const updatedUser = await userRepository.createQueryBuilder("user")
+  .where("user.id = :id_user",{id_user:id})
+  .leftJoinAndSelect("user.skills","skills")
+  .getOne();
 
-    return await createUserResponseSerializer.validate(updatedUser, {
-        stripUnknown: true,
-      });
+  return await createUserResponseSerializer.validate(updatedUser, {
+      stripUnknown: true,
+    });
 }
