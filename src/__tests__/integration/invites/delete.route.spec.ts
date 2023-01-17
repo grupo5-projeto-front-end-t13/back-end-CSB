007c3abc26d3a220c62d862aba7818bdf5bd8179
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-17 10:26:06 -0300

    fix: fix tests invites and create middleware for delete invites permissions

--- src/__tests__/integration/invites/delete.route.spec.ts
+++ src/__tests__/integration/invites/delete.route.spec.ts
@@ -6,7 +6,7 @@ import { inviteRepository } from "../../../repositories/inviteRepository";
 import { userRepository } from "../../../repositories/userRepository";
 import { mockedBand1, mockedBand1Login, mockedMusician1,mockedUserNotAdmRequest,mockedLoginNotAdmRequest,mockedUserAdmRequest,mockedLoginAdmRequest } from "../../mocks";
 
-describe("Create invite route tests", ()=>{
+describe("Create invite route tests", () => {
   let conn: DataSource;
   const baseUrl: string = "/invites"
 
@@ -20,26 +20,25 @@ describe("Create invite route tests", ()=>{
     await conn.destroy()
   });
 
-  beforeEach(async()=> {
+  beforeEach(async() => {
     const invites = await inviteRepository.find()
     await inviteRepository.remove(invites)
     const users = await userRepository.find()
     await userRepository.remove(users)
-  })
+  });
 
-  it("should be able to delete invites", async()=>{
+  it("should be able to delete invites", async() => {
     const user1 = await request(app).post("/users").send(mockedBand1);
     const user2 = await request(app).post("/users").send(mockedMusician1);
     const loginUser1 = await request(app).post("/login").send(mockedBand1Login)
-    const invite = await request(app).post(baseUrl).send({userIdSend: {id: user2.body.id},
-      userIdReceive: {id: user1.body.id}},).set("Authorization", `Bearer ${loginUser1.body.token}`);
+    const invite = await request(app).post(baseUrl).send({userIdSend: {id: user1.body.id},
+      userIdReceive: {id: user2.body.id}}).set("Authorization", `Bearer ${loginUser1.body.token}`);
     const response = await request(app).delete(`${baseUrl}/${invite.body.id}`).set("Authorization", `Bearer ${loginUser1.body.token}`)
-
+      
     expect(response.status).toBe(204)
+  });
 
-  })
-
-  it("adm should be able to delete invites that not own", async()=>{
+  it("adm should not be able to delete invites that not own", async() => {
     const user1 = await request(app).post("/users").send(mockedBand1);
     const user2 = await request(app).post("/users").send(mockedMusician1); 
     const userAdm = await request(app).post("/users").send(mockedUserAdmRequest);
@@ -51,10 +50,11 @@ describe("Create invite route tests", ()=>{
     userIdReceive: {id: user1.body.id}},).set("Authorization", `Bearer ${loginUser1.body.token}`);
     const response = await request(app).delete(`${baseUrl}/${invite.body.id}`).set("Authorization", `Bearer ${loginAdm.body.token}`)
 
-    expect(response.status).toBe(204)
-  })
+    expect(response.status).toBe(401),
+    expect(response.body).toHaveProperty("message")
+  });
 
-  it("should not be able to delete invites without token", async()=>{
+  it("should not be able to delete invites without token", async() => {
     const user1 = await request(app).post("/users").send(mockedBand1);
     const user2 = await request(app).post("/users").send(mockedMusician1);
     const loginUser1 = await request(app).post("/login").send(mockedBand1Login)
@@ -63,10 +63,10 @@ describe("Create invite route tests", ()=>{
     const response = await request(app).delete(`${baseUrl}/${invite.body.id}`)
 
     expect(response.status).toBe(401)
+    expect(response.body).toHaveProperty("message")
+  });
 
-  })
-
-  it("should not be able to delete invites with invalid id", async()=>{
+  it("should not be able to delete invites with invalid id", async() => {
     const user1 = await request(app).post("/users").send(mockedBand1);
     const loginUser1 = await request(app).post("/login").send(mockedBand1Login)
     
@@ -74,10 +74,9 @@ describe("Create invite route tests", ()=>{
 
     expect(response.status).toBe(404)
     expect(response.body).toHaveProperty("message")
+  });
 
-  })
-
-  it("should not be able to delete invites if not owner or adm", async()=>{
+  it("should not be able to delete invites if not owner", async() => {
     const user1 = await request(app).post("/users").send(mockedBand1);
     const user2 = await request(app).post("/users").send(mockedMusician1); 
     const userNotAdm = await request(app).post("/users").send(mockedUserNotAdmRequest);
@@ -91,5 +90,5 @@ describe("Create invite route tests", ()=>{
 
     expect(response.status).toBe(401)
     expect(response.body).toHaveProperty("message")
-  })
+  });
 })
