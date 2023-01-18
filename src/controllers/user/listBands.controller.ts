91c02e52f37579fd845c55aa276c78cd0da37847
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-01-18 12:42:10 -0300

    feat: Add verification listUser type is different user type

--- src/controllers/user/listBands.controller.ts
+++ src/controllers/user/listBands.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { listBandsService } from "../../services/user/listBands.service";
 
 export const listBandsController = async (req: Request, res: Response) => {
-  const bands = await listBandsService();
+  const userType = req.user.type;
+  const bands = await listBandsService(userType);
   return res.status(200).json(bands);
 };
