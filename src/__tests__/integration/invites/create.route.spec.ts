1ce6628114eae1904ba874586d209b6e188de2b9
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-18 15:54:13 -0300

    fix: fix create route invites tests and add new test

--- src/__tests__/integration/invites/create.route.spec.ts
+++ src/__tests__/integration/invites/create.route.spec.ts
@@ -32,6 +32,39 @@ describe("Create invite route tests", () => {
 
   it("Should be able to create a invite", async () => {
     const userAdm = await request(app).post("/users").send(mockedUserAdmRequest);
+    const verify = await request(app).get(`/users/verify/${userAdm.body.id}`);
+    const loginAdm = await request(app).post("/login").send(mockedLoginAdmRequest);
+    const skills = await request(app).post("/skills").send({ name: "tecladista2" }).set("Authorization", `Bearer ${loginAdm.body.token}`);
+
+    const user1 = await request(app).post("/users").send({...mockedBand1,skills:{id:skills.body.id}});
+    await request(app).get(`/users/verify/${user1.body.id}`);
+    const user2 = await request(app).post("/users").send({...mockedMusician1,skills:{id:skills.body.id}});
+    await request(app).get(`/users/verify/${user2.body.id}`);
+
+    const loginUser1 = await request(app).post("/login").send(mockedBand1Login);
+
+    const response = await request(app)
+      .post(baseUrl)
+      .send({
+        userIdSend: { id: user1.body.id },
+        userIdReceive: { id: user2.body.id },
+      })
+      .set("Authorization", `Bearer ${loginUser1.body.token}`);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(
+      expect.objectContaining({ id: expect.any(String) })
+    );
+    expect(response.body).toEqual(
+      expect.objectContaining({ createdAt: expect.any(String) })
+    );
+
+    const [invites, amount] = await inviteRepository.findAndCount();
+    expect(amount).toBe(1);
+  });
+
+  it("Should not be able to create a invite not verified", async () => {
+    const userAdm = await request(app).post("/users").send(mockedUserAdmRequest);
     const loginAdm = await request(app).post("/login").send(mockedLoginAdmRequest);
     const skills = await request(app).post("/skills").send({ name: "tecladista2" }).set("Authorization", `Bearer ${loginAdm.body.token}`);
 
@@ -56,17 +89,20 @@ describe("Create invite route tests", () => {
       expect.objectContaining({ createdAt: expect.any(String) })
     );
 
-    const [invites, amount] = await inviteRepository.findAndCount();
-    expect(amount).toBe(1);
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty("message");
   });
 
   it("Should not be able to create a invite without token", async () => {
     const userAdm = await request(app).post("/users").send(mockedUserAdmRequest);
+    const verify = await request(app).get(`/users/verify/${userAdm.body.id}`);
     const loginAdm = await request(app).post("/login").send(mockedLoginAdmRequest);
     const skills = await request(app).post("/skills").send({ name: "tecladista2" }).set("Authorization", `Bearer ${loginAdm.body.token}`);
 
     const user1 = await request(app).post("/users").send({...mockedBand1,skills:{id:skills.body.id}});
+    await request(app).get(`/users/verify/${user1.body.id}`);
     const user2 = await request(app).post("/users").send({...mockedMusician1,skills:{id:skills.body.id}});
+    await request(app).get(`/users/verify/${user2.body.id}`);
 
     const response = await request(app)
       .post(baseUrl)
@@ -84,11 +120,14 @@ describe("Create invite route tests", () => {
 
   it("Should not be able to create a invite already exists", async () => {
     const userAdm = await request(app).post("/users").send(mockedUserAdmRequest);
+    const verify = await request(app).get(`/users/verify/${userAdm.body.id}`);
     const loginAdm = await request(app).post("/login").send(mockedLoginAdmRequest);
     const skills = await request(app).post("/skills").send({ name: "tecladista2" }).set("Authorization", `Bearer ${loginAdm.body.token}`);
 
     const user1 = await request(app).post("/users").send({...mockedBand1,skills:{id:skills.body.id}});
+    await request(app).get(`/users/verify/${user1.body.id}`);
     const user2 = await request(app).post("/users").send({...mockedMusician1,skills:{id:skills.body.id}});
+    await request(app).get(`/users/verify/${user2.body.id}`);
 
     const loginUser1 = await request(app).post("/login").send(mockedBand1Login);
     
